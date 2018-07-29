145ac382161d2ea6fea80594043b331e33713547
PinguinoSod <pinguino.sod@gmail.com>
2018-07-29 03:25:10 +0200

    gh-pages initial commit

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import AppList from './apps/AppList.vue';
-import About from './components/About.vue';
-import Application from './apps/Application.vue';
-
-export const routes = [
-  { path: '', component: AppList },
-  { path: '/about', component: About },
-  { path: '/apps/:app', component: Application }
-]
